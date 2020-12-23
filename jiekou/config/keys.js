f84ed03e98c69e353bb246d5d224875875f0c12b
ixinyu <43402453+ixinyu@users.noreply.github.com>
2020-12-23 21:25:24 +0800

    Update keys.js

--- jiekou/config/keys.js
+++ jiekou/config/keys.js
@@ -2,11 +2,11 @@ module.exports = {
   mongoURI: 'mongodb://127.0.0.1:27017/mongodome',
   secretOrkey: 'secret',
   wx:{ //小程序
-    appId:'wx02739ad172c01807',
-    appSecret:'35a978bd4e35fff100cee210058f8360'
+    appId:'xxxx',
+    appSecret:'xxxxx'
   },
   mch:{ //商户号
-    mch_id:1586820961,
-    key:'41914f15f59dc19fef30385f46eb4ce4'
+    mch_id:xxx,
+    key:'xxxxxx'
   }
 }
